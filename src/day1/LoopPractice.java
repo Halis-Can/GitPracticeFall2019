50630134b103a64d652d4f0e459674006c92511a
Halis-Can <haliscan@hotmail.com>
2020-02-29 15:51:55 -0500

    thirt one

--- src/day1/LoopPractice.java
+++ src/day1/LoopPractice.java
@@ -4,5 +4,6 @@ public class LoopPractice {
     public static void main(String[] args) {
         System.out.println("loop practice");
         System.out.println("second modify");
+        System.out.println("thirt modify");
     }
 }
