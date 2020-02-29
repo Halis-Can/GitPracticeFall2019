50afc9eb436970becc8f84ced65abca188b2d7e7
Halis-Can <haliscan@hotmail.com>
2020-02-29 15:42:59 -0500

    second modify

--- src/day1/LoopPractice.java
+++ src/day1/LoopPractice.java
@@ -3,5 +3,6 @@ package day1;
 public class LoopPractice {
     public static void main(String[] args) {
         System.out.println("loop practice");
+        System.out.println("second modify");
     }
 }
