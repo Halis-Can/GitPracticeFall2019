61076f7e40bd8aaf2ad9b9fe77519659fd44690d
Halis-Can <haliscan@hotmail.com>
2020-02-29 15:03:00 -0500

    new changes5

--- src/day1/StringPractice.java
+++ src/day1/StringPractice.java
@@ -2,7 +2,7 @@ package day1;
 
 public class StringPractice {
     public static void main(String[] args) {
-        System.out.println("Hello World4");
+        System.out.println("Hello World5");
     }
 
 }
