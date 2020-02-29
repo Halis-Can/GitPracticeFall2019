3d8f159e298b22f3dd83fbc661561cdc675e55b2
Halis-Can <haliscan@hotmail.com>
2020-02-29 15:27:46 -0500

    new modif

--- src/day1/StringPractice.java
+++ src/day1/StringPractice.java
@@ -3,6 +3,7 @@ package day1;
 public class StringPractice {
     public static void main(String[] args) {
         System.out.println("Hello World7");
+        System.out.println("Java is fan");
     }
 
 }
